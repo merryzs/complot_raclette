001ba063714ded4e70a6f21c3434af6cd9ee2e3a
cdidier <cdidier@gaming.tech>
2025-11-14 09:39:04 +0100

    Ajoutez des fichiers projet.

---
+++ complot_raclette/complot_raclette.cpp
@@ -0,0 +1,11 @@
+#include "framework.h"
+#include "complot_raclette.h"
+
+using namespace std;
+
+#define MAX_LOADSTRING 100
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+
+	switch (msg)
+}
