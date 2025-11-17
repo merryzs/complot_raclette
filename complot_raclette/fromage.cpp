6c1c44fb127911434458106bc8ade1d86e018a0b
cdidier <cdidier@gaming.tech>
2025-11-17 09:40:03 +0100

    regle

--- complot_raclette/fromage.cpp
+++ complot_raclette/fromage.cpp
@@ -2,133 +2,75 @@
 
 using namespace std;
 
-
+// --- Variables globales ---
 int g_width = 600;
 int g_height = 400;
+
 HBITMAP hBitmap = NULL;
-BYTE* pBits = nullptr; // pointeur vers les pixels
-BITMAPINFO bih = {};
-HWND hTextBox = NULL;
-HWND hButton = NULL;
-
-HMENU Mebar;
-HMENU MeFile;
-HMENU MeHelp;
-HBITMAP hbit;
 
 
-// Fonction pour charger un BMP en mémoire avec CreateDIBSection
-bool LoadBitmapDIBSection(const wchar_t* filename, HDC hdc)
-{
-    HANDLE hFile = CreateFileW(filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
-    if (hFile == INVALID_HANDLE_VALUE) return false;
-
-    BITMAPFILEHEADER bfh;
-    DWORD bytesRead;
-    ReadFile(hFile, &bfh, sizeof(bfh), &bytesRead, NULL);
-
-    BITMAPINFOHEADER bmih;
-    ReadFile(hFile, &bmih, sizeof(bmih), &bytesRead, NULL);
-
-    bih.bmiHeader = bmih;
-
-   
-
-    // Lire les pixels
-    SetFilePointer(hFile, bfh.bfOffBits, NULL, FILE_BEGIN);
-    ReadFile(hFile, pBits, bmih.biSizeImage, &bytesRead, NULL);
-
-    CloseHandle(hFile);
-    return true;
-}
-
+// --- Fenêtre callback ---
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg)
     {
     case WM_CREATE:
-    {
-        HDC hdc = GetDC(hwnd);
-        hbit = (HBITMAP)LoadImageW(
+        // Charger l'image ici pour être sûr que le chemin est bon
+        hBitmap = (HBITMAP)LoadImageW(
             NULL,
-
             L"complot_raclette/asset/raclette.bmp",
-
             IMAGE_BITMAP,
             0, 0,
             LR_LOADFROMFILE
         );
 
-
-
         if (!hBitmap)
             MessageBox(hwnd, _T("Impossible de charger raclette.bmp"), _T("img de mrd"), MB_OK);
 
-
+        break; // ← IMPORTANT !!!
 
     case WM_SIZE:
         g_width = LOWORD(lParam);
         g_height = HIWORD(lParam);
-        InvalidateRect(hwnd, NULL, TRUE);
-        break;
 
-    case WM_COMMAND:
-    {
-        int wmId = LOWORD(wParam);
-
-        switch (wmId)
         {
-        case 102: // Bouton "Créer"
-        {
-            wchar_t buffer[256];
-            GetWindowTextW(hTextBox, buffer, 256);
-            MessageBoxW(hwnd, buffer, L"Contenu de la text box", MB_OK);
-            break;
-        }
-        case 103: // Bouton "Quitter"
-        {
-            DestroyWindow(hwnd); // ferme la fenêtre
-            break;
-        }
-
-        // Menu (exemple)
-        case 201:
-            MessageBoxW(hwnd, L"Option 1 sélectionnée", L"Menu", MB_OK);
-            break;
-        case 202:
-            MessageBoxW(hwnd, L"A propos...", L"Aide", MB_OK);
-            break;
-        case 203:
-            MessageBoxW(hwnd, L"Fond sélectionné", L"Aide", MB_OK);
-            break;
-
-        default:
-            return DefWindowProc(hwnd, msg, wParam, lParam);
+            TCHAR title[100];
+            wsprintf(title, _T("raclette - %dx%d"), g_width, g_height);
+            SetWindowText(hwnd, title);
         }
         break;
-    }
 
     case WM_PAINT:
     {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
 
-        HDC hdcMem = CreateCompatibleDC(hdc);
-        HBITMAP oldBitmap = (HBITMAP)SelectObject(hdcMem, hBitmap);
+        if (hBitmap)
+        {
+            HDC memDC = CreateCompatibleDC(hdc);
+            SelectObject(memDC, hBitmap);
 
-        BITMAP bmp;
-        GetObject(hBitmap, sizeof(BITMAP), &bmp);
+            BITMAP bm;
+            GetObject(hBitmap, sizeof(bm), &bm);
 
-        BitBlt(hdc, 0, 0, bmp.bmWidth, bmp.bmHeight, hdcMem, 0, 0, SRCCOPY);
+            BitBlt(
+                hdc,
+                0, 0, bm.bmWidth, bm.bmHeight,
+                memDC,
+                0, 0,
+                SRCCOPY
+            );
 
-        SelectObject(hdcMem, oldBitmap);
-        DeleteDC(hdcMem);
+            DeleteDC(memDC);
+        }
 
         EndPaint(hwnd, &ps);
+        return 0;
     }
 
     case WM_DESTROY:
-        if (hBitmap) DeleteObject(hBitmap);
+        if (hBitmap)
+            DeleteObject(hBitmap);
         PostQuitMessage(0);
         break;
 
@@ -139,25 +81,25 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+
+// --- Point d'entrée ---
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine, int nCmdShow)
 {
-    WNDCLASSW wc = {};
+    WNDCLASS wc = { 0 };
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
-    wc.lpszClassName = L"MaFenetre";
+    wc.lpszClassName = _T("MaFenetre");
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 
-    if (!RegisterClassW(&wc))
-    {
-        MessageBoxW(NULL, L"Erreur lors de l'enregistrement de la classe", L"Erreur", MB_OK | MB_ICONERROR);
-        return 0;
-    }
+    wc.hbrBackground = CreateSolidBrush(RGB(79, 34, 6)); // fond brun raclette
 
-    HWND hwnd = CreateWindowExW(
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
         0,
         wc.lpszClassName,
-        L"Fenêtre Raclette",
+        _T("Fenêtre initiale"),
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT,
         g_width, g_height,
